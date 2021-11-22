e95c4644b0340ea359675a84801f7b4d7bf1a1ca
Muhammad Dony Mulya <vveirt@gmail.com>
2021-11-22 16:42:33 +0800

    test(app): should return pong

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -15,8 +15,8 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return "{message: "pong"}"', () => {
+      expect(appController.ping()).toStrictEqual({ message: 'pong' });
     });
   });
 });
