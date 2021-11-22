2a7edfd9284cd96967d57a8b1b625f680e2396b0
Muhammad Dony Mulya <vveirt@gmail.com>
2021-11-22 18:09:05 +0800

    refactor(path): add path alias for @config

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,7 @@ import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import TypeOrmConfig from './config/typeorm.config';
+import TypeOrmConfig from '@config/typeorm.config';
 
 @Module({
   imports: [
